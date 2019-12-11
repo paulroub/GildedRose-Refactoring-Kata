b2415559518506d1433d8167e6e45c0966fc9671
Paul Roub <paul.roub@proactivedigitalinsights.com>
2019-12-11 12:48:51 -0500

    Moderate refactoring

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,9 +9,13 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 break
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    item.quality = item.quality - 1
+                    if item.sell_in > 0:
+                        item.quality = item.quality - 1
+                    else:
+                        item.quality = item.quality - 2
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -22,14 +26,12 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
+
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
