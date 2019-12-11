1876d5e812d6bc77ce05760d727d9755c7b162bf
Paul Roub <paul.roub@proactivedigitalinsights.com>
2019-12-11 12:27:39 -0500

    More quality coverage

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,11 +4,29 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_normal_quality_decay(self):
+        items = [Item("foo", 7, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        self.assertEquals(49, items[0].quality)
+
+    def test_double_quality_decay_after_sell_by(self):
+        items = [Item("foo", 0, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(48, items[0].quality)
+
+    def test_increasing_cheese_quality(self):
+        items = [Item("Aged Brie", 5, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(2, items[0].quality)
+
+    def test_cheese_quality_capped(self):
+        items = [Item("Aged Brie", 5, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
