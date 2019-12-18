3a4cfa54c97696716698af9752a0864c38016a08
Paul Roub <paul.roub@hidigital.solutions>
2019-12-18 12:28:14 -0500

    Refactor item quality checks

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -17,21 +17,17 @@ class GildedRose(object):
 
 
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.sell_in > 0:
-                        item.quality = item.quality - degradation_rate
-                    else:
-                        item.quality = item.quality - degradation_rate * 2
+                if item.sell_in > 0:
+                    item.quality = item.quality - degradation_rate
+                else:
+                    item.quality = item.quality - degradation_rate * 2
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        item.quality = item.quality + 1
 
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -39,10 +35,10 @@ class GildedRose(object):
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         item.quality = 0
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + degradation_rate
+                    item.quality = item.quality + degradation_rate
 
             item.quality = max(0, item.quality)
+            item.quality = min(50, item.quality)
 
 class Item:
     def __init__(self, name, sell_in, quality):
