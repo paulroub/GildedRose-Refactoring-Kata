d878ba4febf1d81cd4222f4caae36423ade48343
Paul Roub <paul.roub@hidigital.solutions>
2019-12-18 12:35:49 -0500

    Refactored

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -16,26 +16,27 @@ class GildedRose(object):
                 degradation_rate = 1
 
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Aged Brie":
+                item.quality = item.quality + 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 6:
+                    item.quality = item.quality + 3
+                elif item.sell_in < 11:
+                    item.quality = item.quality + 2
+                else:
+                    item.quality = item.quality + 1
+            else:
                 if item.sell_in > 0:
                     item.quality = item.quality - degradation_rate
                 else:
                     item.quality = item.quality - degradation_rate * 2
-            else:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
 
             item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = 0
-                else:
+                if item.name == "Aged Brie":
                     item.quality = item.quality + degradation_rate
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
 
             item.quality = max(0, item.quality)
             item.quality = min(50, item.quality)
